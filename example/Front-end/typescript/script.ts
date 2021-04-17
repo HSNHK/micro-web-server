e73f6feb757fe3649ff0ea5022af2348ccaa5af6
HSNHK <hsnmm1379@gmail.com>
2021-04-17 17:31:23 +0430

    Method of adding information added

--- example/Front-end/typescript/script.ts
+++ example/Front-end/typescript/script.ts
@@ -8,15 +8,34 @@ class Main{
     constructor(url:string){
         this.url = url
         var btnSearch: any = document.getElementById("btn-search")
-        var btnReload: any = document.getElementById("btn-reload")
         var txtSearch: any = document.getElementById("txt-search")
         btnSearch.onclick = (e: Event) => {
             this.search(txtSearch?.value)
         }
+        var btnReload: any = document.getElementById("btn-reload")
         btnReload.onclick=(e:Event)=>{
-           this.get().then((data)=>{
-               this.show(data)
-           })
+            this.get().then((data)=>{
+                this.show(data)
+            })
+        }
+        var txtFirstname: any = document.getElementById("txt-first")
+        var txtLastname: any = document.getElementById("txt-last")
+        var txtAddress: any = document.getElementById("txt-address")
+        var txtEmail: any = document.getElementById("txt-email")
+        var btnCreate: any = document.getElementById("btn-create")
+        btnCreate.onclick=(e:Event)=>{
+            if(txtFirstname.value.length >1
+            && txtLastname.value.length >1
+            && txtAddress.value.length >1
+            && txtEmail.value.length >1){
+                const data:object={
+                    Firstname:txtFirstname.value,
+                    Lastname:txtLastname.value,
+                    Address:txtAddress.value,
+                    Email:txtEmail.value
+                }
+                this.create(data)
+            }
         }
     }
     get(): Promise<any> {
@@ -31,6 +50,25 @@ class Main{
                 }
             })
         })
+    }
+    private add(data:Iinformation):void{
+        var table: any = document.getElementById("tb_main");
+        var len:number = table.rows.length
+        var row = table.insertRow(len)
+        row.setAttribute('id', `row${data.Id}`)
+        row.insertCell(0).innerHTML = len;
+        row.insertCell(1).innerHTML = data.Firstname;
+        row.insertCell(2).innerHTML = data.Lastname;
+        row.insertCell(3).innerHTML = data.Address;
+        row.insertCell(4).innerHTML = data.Email;
+        row.insertCell(5).innerHTML = data.time;
+        var btn:HTMLElement = document.createElement('button');
+        btn.textContent = 'Delete';
+        btn.setAttribute('class','btn btn-danger')
+        btn.onclick = (e) => {
+            this.delete(data.Id)
+        }
+        row.insertCell(6).appendChild(btn);
     }
     clearTable():void{
         var table: any = document.getElementById("tb_main");
@@ -48,25 +86,9 @@ class Main{
     show(data: Array<Iinformation>): void{
         var rowCount: number = 1
         var table: any = document.getElementById("tb_main");
-        //clear old item
         this.clearTable()
         data.forEach((element:Iinformation) => {
-            var row = table.insertRow(rowCount)
-            row.setAttribute('id', `row${element.Id}`)
-            row.insertCell(0).innerHTML = rowCount;
-            row.insertCell(1).innerHTML = element.Firstname;
-            row.insertCell(2).innerHTML = element.Lastname;
-            row.insertCell(3).innerHTML = element.Address;
-            row.insertCell(4).innerHTML = element.Email;
-            row.insertCell(5).innerHTML = element.time;
-            var btn:HTMLElement = document.createElement('button');
-            btn.textContent = 'Delete';
-            btn.setAttribute('class','btn btn-danger')
-            btn.onclick = (e) => {
-                this.delete(element.Id)
-            }
-            row.insertCell(6).appendChild(btn);
-            rowCount++
+            this.add(element)
         });
     }
     search(name: string): void {
@@ -74,6 +96,16 @@ class Main{
             fetch(`${this.url}find?name=${name}`)
                .then(response =>response.json().then(data => this.show(data)))
         }
+    }
+    create(data:object):void{
+        fetch(`${this.url}create`,{
+            method:"POST",
+            body:JSON.stringify(data)
+        }).then((value:Response)=>{
+            value.json().then((data)=>{
+                 this.add(data)
+            })
+        })
     }
 }
 
